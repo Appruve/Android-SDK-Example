ce7517a5eb4a22e0187e68c914f46f989c5c00f8
niinyarko <niinyarko@yahoo.com>
2020-09-17 23:24:09 +0000

    Update example for version 0.0.3

--- app/src/main/java/co/appruve/smartbankdemo/MainActivity.kt
+++ app/src/main/java/co/appruve/smartbankdemo/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import co.appruve.identitysdk.*
 import com.google.android.material.button.MaterialButton
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private lateinit var button: MaterialButton
 
@@ -17,19 +18,10 @@ class MainActivity : AppCompatActivity() {
         button = findViewById(R.id.button)
 
         button.setOnClickListener {
-            val intent = Intent(this, AppruveActivity::class.java)
+            val intent = Intent(this, VerificationActivity::class.java)
             val bundle = Bundle()
 
             bundle.putString(APPRUVE_API_TOKEN, "YOUR API TOKEN")
-            bundle.putString(STATUS_BAR_COLOR, "#00693C")
-            bundle.putString(TOOLBAR_BACKGROUND_COLOR, "#0F5738")
-            bundle.putString(NAVIGATION_BUTTON_BACKGROUND_COLOR, "#75AF96")
-            bundle.putString(ICON_COLOR, "#75AF96")
-            bundle.putString(ICON_TEXT_COLOR, "#292f33")
-            bundle.putString(NORMAL_TEXT_COLOR, "#292f33")
-            bundle.putString(HEADER_COLOR, "#292f33")
-            bundle.putString(SUB_HEADER_COLOR, "#292f33")
-            bundle.putString(TEXT_LABEL_COLOR, "#00693C")
             bundle.putBoolean(IS_GHANA_ENABLED, true)
             bundle.putBoolean(IS_NIGERIA_ENABLED, true)
             bundle.putBoolean(IS_KENYA_ENABLED, true)
@@ -45,9 +37,21 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == START_VERIFICATION_ACTIVITY_REQUEST) {
             // Make sure the request was successful
             if (resultCode == RESULT_SUCCESS_CODE) {
-                if(data != null) {
-                    val verified = data.extras?.getBoolean(APPRUVE_EXTRA_IS_VERIFIED)
-                    Log.e(TAG, verified?.toString() ?: "NONE")
+                if (data != null) {
+                    val isVerified: Boolean =
+                        data.extras?.getBoolean(APPRUVE_EXTRA_IS_VERIFIED, false)!!
+                    val documentType: String =
+                        data.extras?.getString(APPRUVE_EXTRA_DOCUMENT_TYPE, "")!!
+                    val idPhotoUrl = data.extras?.getString(APPRUVE_EXTRA_ID_PHOTO_URL, "")!!
+                    val selfiePhotoUrl =
+                        data.extras?.getString(APPRUVE_EXTRA_SELFIE_PHOTO_URL, "")!!
+                    val verificationId =
+                        data.extras?.getString(APPRUVE_EXTRA_VERIFICATION_ID, "")!!
+                    Log.e(TAG, isVerified.toString())
+                    Log.d(TAG, documentType)
+                    Log.d(TAG, idPhotoUrl)
+                    Log.d(TAG, selfiePhotoUrl)
+                    Log.d(TAG, verificationId)
                 }
             }
         } else {

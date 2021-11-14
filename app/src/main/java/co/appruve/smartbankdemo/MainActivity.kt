3e83c04424be34f5489d7864a6eba2b8508906a3
Laud Bruce-Tagoe <niinyarko@yahoo.com>
2021-11-14 17:56:58 +0000

    Update example for v0.1.4.

--- app/src/main/java/co/appruve/smartbankdemo/MainActivity.kt
+++ app/src/main/java/co/appruve/smartbankdemo/MainActivity.kt
@@ -3,13 +3,15 @@ package co.appruve.smartbankdemo
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import co.appruve.identitysdk.*
 import com.google.android.material.button.MaterialButton
 
-@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private lateinit var button: MaterialButton
+    private lateinit var kycActivityLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,40 +19,9 @@ class MainActivity : AppCompatActivity() {
 
         button = findViewById(R.id.button)
 
-        button.setOnClickListener {
-            val intent = Intent(this, VerificationActivity::class.java)
-            val bundle = Bundle()
-
-            val customParamsBundle = Bundle()
-            customParamsBundle.putString("userId", "0caa730e-0f80-4967-b19c-fe9cdbcb80fa")
-
-            bundle.putString(APPRUVE_API_TOKEN, "YOUR API TOKEN")
-            bundle.putBoolean(IS_RETRY_ENABLED, false) // Disable retry con OCR failure
-            bundle.putBoolean(IS_ID_CAPTURE_ONLY, false) // Capture only ID Document
-            bundle.putBoolean(IS_SELFIE_CAPTURE_ONLY, true) // Capture only Selfie
-            // Set transaction ref previously returned to re-enroll or continue with same verification
-            bundle.putString(TRANSACTION_REFERENCE, "b1543d5d-fc98-41ac-a9fa-ff9be2a159b6")
-
-            bundle.putBoolean(IS_GHANA_ENABLED, true)
-            bundle.putBoolean(IS_NIGERIA_ENABLED, true)
-            bundle.putBoolean(IS_KENYA_ENABLED, true)
-            bundle.putBoolean(IS_NIGERIA_VOTER_ID_ENABLED, true)
-            bundle.putBoolean(IS_NIGERIA_PASSPORT_ID_ENABLED, false)
-            bundle.putBoolean(IS_NIGERIA_DRIVER_LICENSE_ID_ENABLED, false)
-            bundle.putBoolean(IS_NIGERIA_NATIONAL_ID_ENABLED, false)
-            bundle.putBundle(CUSTOM_PARAMS, customParamsBundle)
-
-            intent.putExtras(bundle)
-
-            startActivityForResult(intent, START_VERIFICATION_ACTIVITY_REQUEST)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        // Check which request we're responding to
-        if (requestCode == START_VERIFICATION_ACTIVITY_REQUEST) {
-            // the request was successful
-            if (resultCode == RESULT_SUCCESS_CODE) {
+        kycActivityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val data = result.data
+            if (result.resultCode == RESULT_SUCCESS_CODE) {
                 if (data != null) {
                     val isVerified: Boolean =
                         data.extras?.getBoolean(APPRUVE_EXTRA_IS_VERIFIED, false)!!
@@ -61,16 +32,21 @@ class MainActivity : AppCompatActivity() {
                         data.extras?.getString(APPRUVE_EXTRA_SELFIE_PHOTO_URL, "")!!
                     val verificationId =
                         data.extras?.getString(APPRUVE_EXTRA_VERIFICATION_ID, "")!!
-                    Log.e(TAG, isVerified.toString())
+                    val customParams =
+                        data.extras?.getString(APPRUVE_EXTRA_CUSTOM_PARAMS, "")!!
+                    val extractedIDData =
+                        data.extras?.getString(APPRUVE_EXTRA_ID_DATA, "")!!
+
+
+                    Log.d(TAG, isVerified.toString())
                     Log.d(TAG, documentType)
                     Log.d(TAG, idPhotoUrl)
                     Log.d(TAG, selfiePhotoUrl)
                     Log.d(TAG, verificationId)
+                    Log.d(TAG, customParams)
+                    Log.d(TAG, extractedIDData)
                 }
-            }
-            // the request failed
-            // expect this result if you disable retry
-            else if (resultCode == RESULT_FAILED_CODE) {
+            } else if (result.resultCode == RESULT_FAILED_CODE) {
                 if (data != null) {
                     val errorMessage: String =
                         data.extras?.getString(APPRUVE_EXTRA_ERROR_DATA, "")!!
@@ -81,14 +57,50 @@ class MainActivity : AppCompatActivity() {
                     Log.d(TAG, verificationId)
                 }
             }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
+        }
+
+        button.setOnClickListener {
+            val intent = Intent(this, VerificationActivity::class.java)
+            val bundle = Bundle()
+
+            bundle.putString(APPRUVE_API_TOKEN, "YOUR_API_TOKEN")
+            bundle.putBoolean(IS_RETRY_ENABLED, true) // Disable retry on OCR failure
+            bundle.putBoolean(IS_ID_CAPTURE_ONLY, false) // Capture only ID Document
+            bundle.putBoolean(IS_SELFIE_CAPTURE_ONLY, false) // Capture only Selfie
+            bundle.putString(TRANSACTION_REFERENCE, "3e639285-22ee-45a3-beb2-43e320d391a1") // Set transaction ref previously returned for re-enrollment
+
+            // enable or disable countries
+            bundle.putBoolean(IS_GHANA_ENABLED, true)
+            bundle.putBoolean(IS_NIGERIA_ENABLED, true)
+            bundle.putBoolean(IS_KENYA_ENABLED, true)
+
+            // enable or disable Ghana IDs
+            bundle.putBoolean(IS_GHANA_VOTER_ID_ENABLED, true)
+            bundle.putBoolean(IS_GHANA_PASSPORT_ID_ENABLED, true)
+            bundle.putBoolean(IS_GHANA_DRIVER_LICENSE_ID_ENABLED, true)
+            bundle.putBoolean(IS_GHANA_SSNIT_ID_ENABLED, true)
+
+            // enable or disable Nigeria IDs
+            bundle.putBoolean(IS_NIGERIA_VOTER_ID_ENABLED, true)
+            bundle.putBoolean(IS_NIGERIA_PASSPORT_ID_ENABLED, true)
+            bundle.putBoolean(IS_NIGERIA_DRIVER_LICENSE_ID_ENABLED, true)
+            bundle.putBoolean(IS_NIGERIA_NATIONAL_ID_ENABLED, true)
+
+            // enable or disable Kenya IDs
+            bundle.putBoolean(IS_KENYA_NATIONAL_ID_ENABLED, true)
+            bundle.putBoolean(IS_KENYA_PASSPORT_ID_ENABLED, true)
+
+            // Attach custom params against the verification record
+            val customParamsBundle = Bundle()
+            customParamsBundle.putString("userId", "a00d2f6b-0af1-4fdd-893c-6d392d1b1176")
+            bundle.putBundle(CUSTOM_PARAMS, customParamsBundle)
+
+            intent.putExtras(bundle)
+            kycActivityLauncher.launch(intent)
         }
     }
 
-
     companion object {
         private const val TAG = "MainActivity"
-        private const val START_VERIFICATION_ACTIVITY_REQUEST = 1
     }
 }
